8705d1b503a58136766484a047a2a3094d955823
ТЁТЯ_ЗИНА <_@DESKTOP-7L5CID9>
2026-03-28 13:46:13 +0300

    Vite + React

---
+++ Project/frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <button
+        className="counter"
+        onClick={() => setCount((count) => count + 1)}
+      >
+        Count is {count}
+      </button>
+    </div>
+  )
+}
+
+export default App
